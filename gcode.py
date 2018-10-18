76f9324fac0c430ab6abfd7d2f38a4180935634c
WFU <wfu@college.harvard.edu>
2018-10-18 12:58:28 -0400

    [proto] skeleton class

---
+++ gcode.py
@@ -0,0 +1,12 @@
+"""Bad GCODE generator.
+@author: wfus 
+
+Simple tool to generate GCode for 3D printers.
+Used for the scanning microphone project.
+"""
+import argparse
+import math
+import numpy as np
+
+class GCodeProgram(object):
+    pass
