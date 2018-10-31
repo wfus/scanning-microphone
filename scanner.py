d9a0fba071710afca40a09021163737e3174fc73
William Fu <wfu@college.harvard.edu>
2018-10-31 17:50:41 -0400

    [scan] added rectangular prism scan

--- scanner.py
+++ scanner.py
@@ -71,6 +71,49 @@ class Scanner(object):
         # Move back to our original location. Important since we are using relative coordinates.
         self.move(x=-distance_x, y=-distance_y)
 
+    def scan_rectangular_prism(self, begin_coord, end_coord, resolution, resolution_z, record_time=2.0, savepath="./data"):
+        """Scans along a square lattice and saves each audio clip at each location. Audio clips
+        will be saved the format <savepath>/<time.time()>_<xloc>_<yloc>_<zloc>.wav
+
+        @param begin_coord: tuple of x, y, z coordinate to start out with. Since the current code
+                            is location agnostic, it is assume the CNC is over begin_coord already
+                            TODO: Write a re-centering script or something lmao
+        @param end_coord: tuple of x, y, z coordinate to scan until
+        @param resolution: number of samples for each dimension. If 10 is selected, we'll scan 100 points.
+        @param savepath: folder that your saved wave files will be sent to.
+        """
+        # Create a folder to store all of our sound samples in
+        print_begin_time = int(time.time())
+        savefolder = os.path.join(savepath, str(print_begin_time))
+        if not os.path.exists(savefolder):
+            os.makedirs(savefolder)
+        # since we are assuming that we start at the begin_coord, consider the relative coordinates where
+        # begin_coord is just the origin already.
+        distance_x = end_coord[0] - begin_coord[0]
+        distance_y = end_coord[1] - begin_coord[1]
+        distance_z = end_coord[2] - begin_coord[2]
+        for z in np.linspace(0, distance_z, resolution_z):
+            scan_points = [(x, y) for x in np.linspace(0, distance_x, resolution) 
+                                for y in np.linspace(0, distance_y, resolution)]
+            
+            # Beginning at the begin_coord, we are doing to stop and keep scanning
+            previous_coord = begin_coord
+            for p_x, p_y in scan_points:
+                dx = p_x - previous_coord[0]
+                dy = p_y - previous_coord[1]
+                self.move(x=dx, y=dy)
+                fname = os.path.join(savefolder, "{}_{}_{}.wav".format(p_x, p_y, z))
+                self.mic.record_to_wav(record_time, fname)
+                previous_coord = p_x, p_y
+
+            # Move back to our original location. Important since we are using relative coordinates.
+            self.move(x=-distance_x, y=-distance_y)
+
+            # Move up Z coordinates
+            self.move(z=(distance_z / resolution_z))
+        
+        # Move back to our original location in z
+        self.move(z=-distance_z)
 
 
     def move(self, x=None, y=None, z=None, delay=MOVEMENT_DELAY_TIME, delay_factor=MOVEMENT_DELAY_MULTIPLIER):
