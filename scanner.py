5a6a6d8f4486a7d3346550d0e072c22d00b652a5
William Fu <wfu@college.harvard.edu>
2019-02-27 11:32:24 -0500

    wip

--- scanner.py
+++ scanner.py
@@ -260,7 +260,55 @@ class Scanner(object):
 
         # Dump relevant information into the same folder in a file called
         # notes or something
+
+    def scan_grid(self, end_coord, resolution, scan_speed=4000, record_time=2.0, 
+        delay=0.5, sample_start=0, sample_end=10000, savepath="./data", note=""):
+        """Scans along a square lattice and saves each audio clip at each location.
+        Audio clips will be saved the format:
+            <savepath>/<time.time()>_<xloc>_<yloc>_<zloc>.wav
+        @param begin_coord: tuple of x and y coordinate to start out with. Since the current code
+                            is location agnostic, it is assume the CNC is over begin_coord already
+                            TODO: Write a re-centering script or something lmao
+        @param end_coord: tuple of x and y coordinate to scan until
+        @param resolution: number of samples for each dimension. If 10 is selected, we'll scan 100 points.
+        @param savepath: folder that your saved wave files will be sent to.
+        """
+        # Create a folder to store all of our sound samples in
+        print_begin_time = int(time.time())
+        savefolder = os.path.join(savepath, str(print_begin_time))
+        if not os.path.exists(savefolder):
+            os.makedirs(savefolder)
+
+        with open(os.path.join(savefolder, 'info'), 'w') as f:
+            f.write("Scanning on grid, stopping at each point. Using parameters\n")
+            f.write('SampleStart: %d\n' % sample_start)
+            f.write('SampleEnd: %d\n' % sample_end)
+            f.write('RecordTime: %d\n' % sample_start)
+            f.write('end_coord: %s\n' % str(end_coord))
+            f.write('resolution: %d\n' % resolution)
+            f.write("\n\n")
+            f.write("Additional notes:\n%s\n" % note)
+
+        # since we are assuming that we start at the begin_coord, consider the relative coordinates where
+        # begin_coord is just the origin already.
+        distance_x = end_coord[0]
+        distance_y = end_coord[1]
+        scan_points = [(x, y) for x in np.linspace(0, distance_x, resolution) 
+                              for y in np.linspace(0, distance_y, resolution)]
         
+        # Beginning at the begin_coord, we are doing to stop and keep scanning
+        previous_coord = (0, 0)
+        for p_x, p_y in tqdm.tqdm(scan_points):
+            dx = p_x - previous_coord[0]
+            dy = p_y - previous_coord[1]
+            self.move_speed(x=dx, y=dy, speed=scan_speed)
+            fname = os.path.join(savefolder, "{}_{}_{}".format(p_x, p_y, 0))
+            self.mic.record_to_file(record_time, fname, delay=delay,
+                sample_start=sample_start, sample_end=sample_end)
+            previous_coord = p_x, p_y
+
+        # Move back to our original location. Important since we are using relative coordinates.
+        self.move_speed(x=-distance_x, y=-distance_y, speed=scan_speed)        
 
 
     def __str__(self):
